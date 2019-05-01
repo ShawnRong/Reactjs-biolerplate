0ec048c1cdad8a27de536783155dc929c7ca3cae
ShawnRong <rongshawn@gmail.com>
2019-05-01 20:10:30 +0800

    Boilerplate init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './App.scss';
+
+const App = () => (
+  <div className="container mx-auto">
+
+    <h1>123</h1>
+  </div>
+)
+
+export default hot(module)(App);
